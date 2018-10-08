f8fc8e228fd8a67e61f2118bada3afcaa61b5f4b
= <=>
2018-10-08 15:29:59 -0500

    added membership name property to MembershipType

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace Vidly.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+
+        [Required] //Makes it so "Name" is not nullable in the DB
+        [StringLength(255)] //Makes it so "Name" can only be up to 255 characters
+        public String MembershipName { get; set; }
+
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
