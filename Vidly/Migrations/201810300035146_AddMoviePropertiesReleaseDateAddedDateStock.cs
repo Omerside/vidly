1c32737c282d1d677123187b064b17e6a8087185
= <=>
2018-10-29 23:11:42 -0500

    updated movies

---
+++ Vidly/Migrations/201810300035146_AddMoviePropertiesReleaseDateAddedDateStock.cs
@@ -0,0 +1,22 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMoviePropertiesReleaseDateAddedDateStock : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "AddedDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "Stock", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "Stock");
+            DropColumn("dbo.Movies", "AddedDate");
+            DropColumn("dbo.Movies", "ReleaseDate");
+        }
+    }
+}
